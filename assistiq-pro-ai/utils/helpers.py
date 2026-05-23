e3ae38bafb0a0e4e64a90a27e7d3c5d087941d5f
varun vutla <varunvutla@varuns-Mac-mini.local>
2026-05-23 12:57:12 +0530

    Fixed CSS file path

--- assistiq-pro-ai/utils/helpers.py
+++ assistiq-pro-ai/utils/helpers.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 import streamlit as st
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 def load_css(css_file):
 
-    with open(css_file) as f:
+    css_path = BASE_DIR / css_file
+
+    with open(css_path) as f:
+
         st.markdown(
             f"<style>{f.read()}</style>",
             unsafe_allow_html=True
